678f978bcd76ae928918c5450ed44624c5358268
Donatien Thorez <donatienthorez@gmail.com>
2015-01-28 12:01:01 +0100

    Modification du css et de la page de login

--- includes/partials/login.php
+++ includes/partials/login.php
@@ -1,33 +1,15 @@
 <div class="account-container">
 	<div class="content clearfix">
-		<form action="index.php" method="post">
-			
-			<h1>Member Login</h1>		
-			<div class="login-fields">
-				<p>Please provide your details</p>
-				
-				<div class="field">
-					<label for="username">Username</label>
-					<input type="text" id="username" name="username" value="" placeholder="Username" class="login username-field" />
-				</div>
-				
-				<div class="field">
-					<label for="password">Password:</label>
-					<input type="password" id="password" name="password" value="" placeholder="Password" class="login password-field"/>
-				</div>
-			</div>
-			 			
-			<div class="login-actions">
-													
-				
-				<button class="button btn btn-success btn-large">Sign In</button>
-				
-			</div>	
-			
-		</form>
-		<a href="login_galaxy.php">
-			<button class="button btn btn-success btn-medium">Connect with Galaxy</button>
-		</a>
 		
+		<h1>Welcome </h1>
+		<div class="login-fields">
+			<p> Welcome to the Survival Guide edition website.</p>
+			<p> To access to all the functionalities, connect with your galaxy account.</p>
+		</div>
+		<div style="text-align:center;">
+		<a href="login_galaxy.php">
+			<button class="button btn btn-success btn-medium btn-galaxy">Connect with Galaxy</button>
+		</a>
+		</div>
 	</div>
 </div>
